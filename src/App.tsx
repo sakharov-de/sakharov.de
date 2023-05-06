e630278aea010fb95d2074926fe0ca871b6b0160
Denis Sakharov <denis.sakharov@icloud.com>
2023-05-06 14:43:33 +0400

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
